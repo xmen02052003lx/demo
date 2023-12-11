1dd7f05e2ad48cf78831eadbba8d576c325020d1
xmen02052003lx <97832906+xmen02052003lx@users.noreply.github.com>
2023-12-11 07:19:11 +0700

    comment out the unneccessary code

--- assets/js/demo.js
+++ assets/js/demo.js
@@ -35,40 +35,40 @@ function getUsers(value, user) {
   )
 }
 
-function getDropdownData(user, type) {
-  if ($(".dropdown_data_window").css("height") == "0px") {
-    let pageName
-    if (type == "notification") {
-    } else if (type == "message") {
-      pageName = "ajax_load_messages.php"
-      $("span").remove("#unread_message")
-    }
+// function getDropdownData(user, type) {
+//   if ($(".dropdown_data_window").css("height") == "0px") {
+//     let pageName
+//     if (type == "notification") {
+//     } else if (type == "message") {
+//       pageName = "ajax_load_messages.php"
+//       $("span").remove("#unread_message")
+//     }
 
-    let ajaxreq = $.ajax({
-      url: "includes/handlers/" + pageName,
-      type: "POST",
-      data: "page=1&userLoggedIn=" + user,
-      cache: false,
+//     let ajaxreq = $.ajax({
+//       url: "includes/handlers/" + pageName,
+//       type: "POST",
+//       data: "page=1&userLoggedIn=" + user,
+//       cache: false,
 
-      success: function (response) {
-        $(".dropdown_data_window").html(response)
-        $(".dropdown_data_window").css({
-          padding: "0px",
-          height: "200px",
-          border: "1px solid #DADADA",
-        })
-        $("#dropdown_data_type").val(type)
-      },
-    })
-  } else {
-    $(".dropdown_data_window").html("")
-    $(".dropdown_data_window").css({
-      padding: "0px",
-      height: "0px",
-      border: "1px solid #DADADA",
-    })
-  }
-}
+//       success: function (response) {
+//         $(".dropdown_data_window").html(response)
+//         $(".dropdown_data_window").css({
+//           padding: "0px",
+//           height: "200px",
+//           border: "1px solid #DADADA",
+//         })
+//         $("#dropdown_data_type").val(type)
+//       },
+//     })
+//   } else {
+//     $(".dropdown_data_window").html("")
+//     $(".dropdown_data_window").css({
+//       padding: "0px",
+//       height: "0px",
+//       border: "1px solid #DADADA",
+//     })
+//   }
+// }
 
 function getLiveSearchUsers(value, user) {
   $.post(
